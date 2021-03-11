bbae98a39a9f96191874c0a764c47674c7e4d03f
hloirea <hloirea@gmail.com>
2021-03-11 22:45:02 +0900

    using layer_state_set_user()

--- keyboards/gh60/revc/keymaps/hloirea/keymap.c
+++ keyboards/gh60/revc/keymaps/hloirea/keymap.c
@@ -8,11 +8,6 @@ enum LAYER_IDX {
   _CL,
   _MAX_LAYER
 };
-#define LT_Q_BL LT(_BL, KC_Q)
-#define LT_W_FL LT(_FL, KC_W)
-#define LT_E_UL LT(_UL, KC_E)
-#define LT_R_ML LT(_ML, KC_R)
-#define LT_T_CL LT(_CL, KC_T)
 
 #define _______ KC_TRNS
 #define xxxxxxx KC_NO
@@ -24,20 +19,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
          KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
          KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_CL),
-         KC_LALT, MO(_FL), KC_LCTL,                   KC_SPC,                     KC_LCTL, MO(_FL), KC_LGUI, KC_RALT),
+         KC_LALT, MO(_FL), KC_LCTL,                   KC_SPC,                     KC_LCTL, KC_LGUI, MO(_FL), KC_RALT),
   /* _FL: Function key Layer */
 	[_FL] =
       LAYOUT_60_ansi_split_bs_rshift(
          _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-         MO(_BL), MO(_FL), MO(_UL), MO(_CL), _______, _______, KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,
+         _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,
          KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______, _______, _______,
-         _______, KC_APP,  _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, MO(_CL),
+         _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, KC_APP,  MO(_CL),
          _______, _______, _______,                   _______,                    _______, _______, _______, _______),
   /* _UL: Utility Layer */
 	[_UL] =
       LAYOUT_60_ansi_split_bs_rshift(
-         xxxxxxx, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, KC_CALC,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______,
-         _______, _______, _______, _______, xxxxxxx, xxxxxxx, KC_NLCK, KC_P7,    KC_P8,   KC_P9,   KC_PSLS, KC_PAST, xxxxxxx, xxxxxxx,
+         _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, KC_CALC,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______,
+         xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_NLCK, KC_P7,    KC_P8,   KC_P9,   KC_PSLS, KC_PAST, xxxxxxx, xxxxxxx,
          KC_BSPC, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_P4,    KC_P5,   KC_P6,   KC_PMNS, KC_PPLS, KC_PENT,
          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_P0,   KC_P1,    KC_P2,   KC_P3,   KC_PDOT, xxxxxxx, MO(_CL),
          _______, _______, _______,                   _______,                    _______, _______, _______, _______),
@@ -45,39 +40,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ML] =
       LAYOUT_60_ansi_split_bs_rshift(
          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
-         _______, _______, _______, _______, xxxxxxx, xxxxxxx, xxxxxxx, KC_WH_D,  KC_WH_U, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
+         xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_WH_D,  KC_WH_U, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
          xxxxxxx, KC_BTN1, KC_BTN2, KC_BTN3, xxxxxxx, xxxxxxx, KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, xxxxxxx, xxxxxxx, xxxxxxx,
          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, MO(_CL),
-         xxxxxxx, _______, xxxxxxx,                   xxxxxxx,                    xxxxxxx, _______, xxxxxxx, xxxxxxx),
+         xxxxxxx, _______, xxxxxxx,                   xxxxxxx,                    xxxxxxx, xxxxxxx, _______, xxxxxxx),
   /* _CL: Control Layer */
 	[_CL] =
   LAYOUT_60_ansi_split_bs_rshift(
          TO(_BL), TO(_FL), TO(_UL), TO(_ML), xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
-         xxxxxxx, _______, _______, _______, _______, _______, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
+         xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______,
-         xxxxxxx, _______, xxxxxxx,                   xxxxxxx,                    xxxxxxx, _______, xxxxxxx, xxxxxxx),
+         xxxxxxx, _______, xxxxxxx,                   xxxxxxx,                    xxxxxxx, xxxxxxx, _______, xxxxxxx),
 };
 
-void matrix_scan_user(void) {
-
-//Layer LED indicators
-    uint32_t layer = layer_state;
-
-    if (layer_state_is(_FL)) {
-        // gh60_poker_leds_on();
-        gh60_fn_led_on();
-    } else {
-        // gh60_poker_leds_off();
-        gh60_fn_led_off();
-    }
-
-    if (layer_state_is(_UL)) {
-        // gh60_wasd_leds_on();
-        gh60_esc_led_on();
-    } else {
-        // gh60_wasd_leds_off();
-        gh60_esc_led_off();
-    }
-
-};
+layer_state_t layer_state_set_user(layer_state_t state) {
+  uint8_t layer = get_highest_layer(state);
+  // esc led
+  if (layer == _BL) {
+    gh60_esc_led_off();
+  } else if (layer == _FL || layer == _UL || layer == _ML) {
+    gh60_esc_led_on();
+  }
+  // fn led
+  if (layer == _CL) {
+    gh60_fn_led_on();
+  } else {
+    gh60_fn_led_off();
+  }
+  return state;
+}
