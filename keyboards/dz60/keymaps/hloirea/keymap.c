6353544a2062742b6409ab544cba4fd4c4b7a93b
seil.lim <seil.lim@grapicar.com>
2021-03-31 17:53:04 +0900

    Modify align

--- keyboards/dz60/keymaps/hloirea/keymap.c
+++ keyboards/dz60/keymaps/hloirea/keymap.c
@@ -3,25 +3,25 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* _BL: Basic Layer */
-	[_BL] = LAYOUT_hloirea_wrapper(__BL_0__,
+    [_BL] = LAYOUT_hloirea_wrapper(__BL_0__,
                                    __BL_1__,
                                    __BL_2__,
                                    __BL_3__,
                                    __BL_4__),
     /* _FL: Function key Layer */
-	[_FL] = LAYOUT_hloirea_wrapper(__FL_0__,
+    [_FL] = LAYOUT_hloirea_wrapper(__FL_0__,
                                    __FL_1__,
                                    __FL_2__,
                                    __FL_3__,
                                    __FL_4__),
     /* _NL: Numpad Layer */
-	[_NL] = LAYOUT_hloirea_wrapper(__NL_0__,
+    [_NL] = LAYOUT_hloirea_wrapper(__NL_0__,
                                    __NL_1__,
                                    __NL_2__,
                                    __NL_3__,
                                    __NL_4__),
     /* _CL: Control Layer */
-	[_CL] = LAYOUT_hloirea_wrapper(__CL_0__,
+    [_CL] = LAYOUT_hloirea_wrapper(__CL_0__,
                                    __CL_1__,
                                    __CL_2__,
                                    __CL_3__,
