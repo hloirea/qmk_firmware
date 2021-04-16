1ecf8df87ea782bbc61e513e5f2b7a341eee6db8
seil.lim <seil.lim@grapicar.com>
2021-04-16 17:32:09 +0900

    change Mod key layout

--- users/hloirea/layout_60_default.h
+++ users/hloirea/layout_60_default.h
@@ -6,7 +6,7 @@
 #define __BL_1__ KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS
 #define __BL_2__ KC_BSPC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT
 #define __BL_3__ KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(_CL)
-#define __BL_4__ KC_LALT,MO(_FL),KC_LCTL,                    KC_SPC,                     KC_RCTL,KC_RGUI,MO(_FL),KC_RALT
+#define __BL_4__ MO(_FL),KC_LALT,KC_LCTL,                    KC_SPC,                     KC_RCTL,KC_RALT,KC_RGUI,KC_APP
 
 #define __FL_0__ _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_INS
 #define __FL_1__ MO(_NL),KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_PSCR,KC_SLCK,KC_PAUS,_______
@@ -15,7 +15,7 @@
 #define __FL_4__ _______,_______,_______,                    _______,                    _______,_______,_______,_______
 
 #define __NL_0__ _______,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_UNDS,KC_EQL, KC_GRV, KC_DEL
-#define __NL_1__ KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_NLCK,KC_P7,  KC_P8,  KC_P9,  KC_PSLS,KC_PAST,KC_RBRC,KC_BSLS
+#define __NL_1__ _______,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_NLCK,KC_P7,  KC_P8,  KC_P9,  KC_PSLS,KC_PAST,KC_RBRC,KC_BSLS
 #define __NL_2__ KC_BSPC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PMNS,KC_PPLS,KC_PENT
 #define __NL_3__ _______,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_P0,  KC_P1,  KC_P2,  KC_P3,  KC_PDOT,_______,_______
 #define __NL_4__ _______,_______,_______,                    _______,                    _______,_______,_______,_______
@@ -24,4 +24,4 @@
 #define __CL_1__ xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx
 #define __CL_2__ xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx
 #define __CL_3__ xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx,_______
-#define __CL_4__ xxxxxxx,_______,xxxxxxx,                    xxxxxxx,                    xxxxxxx,xxxxxxx,_______,xxxxxxx
+#define __CL_4__ _______,xxxxxxx,xxxxxxx,                    xxxxxxx,                    xxxxxxx,xxxxxxx,xxxxxxx,xxxxxxx
