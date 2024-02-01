51f793e364b5886042d3b96ae3a87cd75d943f9e
hloirea <hloirea@gmail.com>
2024-02-01 22:37:47 +0900

    Update Keymap

--- users/hloirea/hloirea.h
+++ users/hloirea/hloirea.h
@@ -2,13 +2,13 @@
 
 #include "layers.h"
 
-#define HLOIREA_LAYER_LIST                     \
-    HLOIREA_LAYER_FORMAT(BASE,    "Base")      \
-    HLOIREA_LAYER_FORMAT(TO_FUNC, "ToFunc")    \
-    HLOIREA_LAYER_FORMAT(FUNC,    "Func")      \
-    HLOIREA_LAYER_FORMAT(TO_NUM,  "ToNum")     \
-    HLOIREA_LAYER_FORMAT(NUM,     "Num")       \
-    HLOIREA_LAYER_FORMAT(MOUSE,   "Mouse")     \
+#define HLOIREA_LAYER_LIST                  \
+    HLOIREA_LAYER_FORMAT(BASE,  "Base")     \
+    HLOIREA_LAYER_FORMAT(FUN,   "Fun")      \
+    HLOIREA_LAYER_FORMAT(NUM,   "Num")      \
+    HLOIREA_LAYER_FORMAT(NAV,   "Nav")      \
+    HLOIREA_LAYER_FORMAT(SYM,   "Sym")      \
+    HLOIREA_LAYER_FORMAT(MOUSE, "Mouse")    \
 
 #if 0 /* advanced tap dances is not currently in use */
 #define HLOIREA_TD_TAP_HOLD                     \
@@ -29,18 +29,17 @@ enum hloirea_layers_e {
 #define U_P1       S(KC_F1)
 #define U_P2       S(KC_F2)
 
-#define U_P_TAB    LT(L_TO_NUM, KC_TAB)
 #define U_P_SPC    LALT_T(KC_SPC)
 #define U_P_P1     LSFT_T(U_P1)
 #define U_P_P2     RSFT_T(U_P2)
 #define U_P_BSPC   RGUI_T(KC_BSPC)
-#define U_P_ENT    LT(L_TO_FUNC, KC_ENT)
 
+#define U_P_LNG1   RCTL_T(KC_LNG1)
+
+#define U_P_UNDO   C(KC_Z)
 #define U_P_CUT    S(KC_DEL)
 #define U_P_COPY   C(KC_INS)
 #define U_P_PSTE   S(KC_INS)
-
-#define U_P_LNG1   RCTL_T(KC_LNG1)
 
 enum custom_keycodes {
     U_MACRO_00 = SAFE_RANGE,
@@ -48,9 +47,5 @@ enum custom_keycodes {
     U_MACRO_02,
     U_MACRO_03,
     U_MACRO_04,
-    U_MACRO_05,
-    U_MACRO_06,
-    U_MACRO_07,
-    U_MACRO_08,
-    U_MACRO_09
+    U_MACRO_05
 };
--- users/hloirea/layers.h
+++ users/hloirea/layers.h
@@ -1,37 +1,37 @@
 #pragma once
 
 #define HLOIREA_LAYER_BASE \
-KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_DEL,    \
-OSL(L_NUM),   KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      OSL(L_FUNC),\
-KC_LCTL,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      U_P_LNG1,  \
-                                          U_P_TAB,      U_P_SPC,      U_P_P1,       U_P_P2,       U_P_BSPC,     U_P_ENT
+OSL(L_FUN),   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         OSL(L_NAV),\
+OSL(L_NUM),   KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      OSL(L_SYM),\
+OSM(MOD_LCTL),KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      U_P_LNG1,  \
+                                          KC_TAB,       U_P_SPC,      U_P_P1,       U_P_P2,       U_P_BSPC,     KC_ENT
 
-#define HLOIREA_LAYER_TO_FUNC \
-XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
-XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      TO(L_FUNC),\
-XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
-                                          XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______
-
-#define HLOIREA_LAYER_FUNC \
-TO(L_BASE),   KC_HOME,      KC_UP,        KC_END,       KC_PGUP,      KC_VOLU,      KC_PSCR,      KC_F7,        KC_F8,        KC_F9,        KC_F12,       TO(L_BASE),\
-KC_UNDS,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_PGDN,      KC_VOLD,      KC_SCRL,      KC_F4,        KC_F5,        KC_F6,        KC_F11,       KC_DQUO,   \
-KC_CAPS,      TD(TD_RESET), U_P_CUT,      U_P_COPY,     U_P_PSTE,     KC_MUTE,      KC_PAUS,      KC_F1,        KC_F2,        KC_F3,        KC_F10,       KC_RCTL,   \
-                                          KC_TAB,       KC_SPC,       KC_LSFT,      KC_RSFT,      KC_BSPC,      KC_ENT
-
-#define HLOIREA_LAYER_TO_NUM \
-XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
-TO(L_NUM),    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
-XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
-                                          _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+#define HLOIREA_LAYER_FUN \
+KC_ESC,       XXXXXXX,      XXXXXXX,      XXXXXXX,      TO(L_BASE),   XXXXXXX,      KC_PSCR,      KC_F7,        KC_F8,        KC_F9,        KC_F12,       KC_CAPS,   \
+KC_UNDS,      XXXXXXX,      XXXXXXX,      TO(L_NAV),    TO(L_FUN),    XXXXXXX,      KC_SCRL,      KC_F4,        KC_F5,        KC_F6,        KC_F11,       KC_DQUO,   \
+KC_LCTL,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_PAUS,      KC_F1,        KC_F2,        KC_F3,        KC_F10,       KC_RCTL,   \
+                                          XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RSFT,      KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_NUM \
-TO(L_BASE),   KC_TILD,      KC_CIRC,      KC_PIPE,      KC_AMPR,      KC_AT,        KC_GRV,       KC_7,         KC_8,         KC_9,         KC_HASH,      TO(L_BASE),\
-KC_UNDS,      KC_EXLM,      KC_LABK,      KC_PEQL,      KC_RABK,      KC_QUES,      KC_QUOT,      KC_4,         KC_5,         KC_6,         KC_DLR,       KC_DQUO,   \
-KC_LCTL,      KC_PSLS,      KC_PPLS,      KC_PMNS,      KC_PAST,      KC_PERC,      KC_0,         KC_1,         KC_2,         KC_3,         KC_BSLS,      KC_LNG2,   \
-                                          KC_TAB,       KC_SPC,       KC_LSFT,      KC_RSFT,      KC_BSPC,      KC_ENT
+KC_ESC,       XXXXXXX,      XXXXXXX,      XXXXXXX,      TO(L_BASE),   XXXXXXX,      XXXXXXX,      KC_7,         KC_8,         KC_9,         KC_HASH,      XXXXXXX,   \
+KC_UNDS,      XXXXXXX,      XXXXXXX,      TO(L_SYM),    TO(L_NUM),    XXXXXXX,      KC_QUOT,      KC_4,         KC_5,         KC_6,         KC_DLR,       KC_DQUO,   \
+KC_LCTL,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_0,         KC_1,         KC_2,         KC_3,         KC_BSLS,      KC_LNG2,   \
+                                          XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RSFT,      KC_BSPC,      KC_ENT
+
+#define HLOIREA_LAYER_NAV \
+KC_ESC,       KC_HOME,      KC_UP,        KC_END,       KC_PGUP,      KC_VOLU,      XXXXXXX,      TO(L_BASE),   XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
+KC_UNDS,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_PGDN,      KC_VOLD,      XXXXXXX,      TO(L_NAV),    TO(L_FUN),    XXXXXXX,      XXXXXXX,      KC_DQUO,   \
+KC_LCTL,      U_P_UNDO,     U_P_CUT,      U_P_COPY,     U_P_PSTE,     KC_MUTE,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RCTL,   \
+                                          KC_TAB,       KC_SPC,       KC_LSFT,      XXXXXXX,      XXXXXXX,      XXXXXXX
+
+#define HLOIREA_LAYER_SYM \
+KC_GRV,       KC_TILD,      KC_CIRC,      KC_PIPE,      KC_AMPR,      KC_AT,        XXXXXXX,      TO(L_BASE),   XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
+KC_UNDS,      KC_EXLM,      KC_LABK,      KC_PEQL,      KC_RABK,      KC_QUES,      XXXXXXX,      TO(L_SYM),    TO(L_NUM),    XXXXXXX,      XXXXXXX,      KC_DQUO,   \
+KC_LCTL,      KC_PSLS,      KC_PPLS,      KC_PMNS,      KC_PAST,      KC_PERC,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RCTL,   \
+                                          KC_TAB,       KC_SPC,       KC_LSFT,      XXXXXXX,      XXXXXXX,      XXXXXXX
 
 #define HLOIREA_LAYER_MOUSE \
-TO(L_BASE),   XXXXXXX,      KC_ACL0,      KC_ACL1,      KC_ACL2,      KC_WH_U,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      TO(L_BASE),\
+XXXXXXX,      XXXXXXX,      KC_ACL0,      KC_ACL1,      KC_ACL2,      KC_WH_U,      XXXXXXX,      TO(L_BASE),   XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
 XXXXXXX,      XXXXXXX,      KC_BTN1,      KC_BTN3,      KC_BTN2,      KC_WH_D,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
 XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,   \
                                           XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
