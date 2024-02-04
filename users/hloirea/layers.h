ce763894a5b72125e6ed946073a4b8e90f80ee0a
hloirea <hloirea@gmail.com>
2024-02-05 02:12:40 +0900

    Update Keymap

--- users/hloirea/layers.h
+++ users/hloirea/layers.h
@@ -19,15 +19,21 @@ OSM(MOD_LCTL),U_P_UNDO,     U_P_CUT,      U_P_COPY,     U_P_PSTE,     KC_MUTE,  
                                           KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_NUM \
-KC_GRV,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_7,         KC_8,         KC_9,         KC_DLR,       KC_DEL,       \
-KC_UNDS,      XXXXXXX,      XXXXXXX,      TO(L_SYM),    TO(L_NUM),    XXXXXXX,      KC_QUOT,      KC_4,         KC_5,         KC_6,         KC_HASH,      KC_DQUO,      \
+KC_GRV,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_HASH,      KC_7,         KC_8,         KC_9,         KC_DLR,       KC_DEL,       \
+KC_UNDS,      XXXXXXX,      XXXXXXX,      TO(L_SYM),    TO(L_NUM),    XXXXXXX,      KC_QUOT,      KC_4,         KC_5,         KC_6,         KC_COLN,      KC_DQUO,      \
 OSM(MOD_LCTL),XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_0,         KC_1,         KC_2,         KC_3,         KC_BSLS,      OSM(MOD_RCTL),\
-                                          KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
+                                          KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_EXT),    KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_SYM \
 KC_GRV,       KC_TILD,      KC_CIRC,      KC_PIPE,      KC_AMPR,      KC_AT,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_DEL,       \
 KC_UNDS,      KC_EXLM,      KC_LABK,      KC_EQL,       KC_RABK,      KC_QUES,      XXXXXXX,      TO(L_SYM),    TO(L_NUM),    XXXXXXX,      XXXXXXX,      KC_DQUO,      \
 OSM(MOD_LCTL),KC_PSLS,      KC_PPLS,      KC_PMNS,      KC_PAST,      KC_PERC,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      OSM(MOD_RCTL),\
+                                          KC_TAB,       KC_SPC,       TO(L_EXT),    TO(L_BASE),   KC_BSPC,      KC_ENT
+
+#define HLOIREA_LAYER_EXT \
+KC_GRV,       KC_TILD,      KC_CIRC,      KC_PIPE,      KC_AMPR,      KC_AT,        KC_HASH,      KC_7,         KC_8,         KC_9,         KC_DLR,       KC_DEL,       \
+KC_UNDS,      KC_EXLM,      KC_LABK,      KC_EQL,       KC_RABK,      KC_QUES,      KC_QUOT,      KC_4,         KC_5,         KC_6,         KC_COLN,      KC_DQUO,      \
+OSM(MOD_LCTL),KC_PSLS,      KC_PPLS,      KC_PMNS,      KC_PAST,      KC_PERC,      KC_0,         KC_1,         KC_2,         KC_3,         KC_BSLS,      OSM(MOD_RCTL),\
                                           KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_MOUSE \
