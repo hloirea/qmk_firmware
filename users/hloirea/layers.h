d4c510d14861dced37313903a450aa2ca74b9b51
hloirea <hloirea@gmail.com>
2024-02-23 21:21:18 +0900

    Update Keymap

--- users/hloirea/layers.h
+++ users/hloirea/layers.h
@@ -1,21 +1,21 @@
 #pragma once
 
 #define HLOIREA_LAYER_BASE \
-KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_DEL,       \
+OSL(L_MOUSE), KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_DEL,       \
 U_P_P1,       KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      U_P_P2,       \
 OSL(L_FUN),   U_P_Z,        KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       U_P_SLSH,     OSL(L_NAV),   \
                                           U_P_TAB,      U_P_SPC,      OSL(L_SYML),  OSL(L_SYMR),  U_P_BSPC,     U_P_ENT
 
 #define HLOIREA_LAYER_FUN \
-KC_ESC,       TD(TD_RESET), XXXXXXX,      XXXXXXX,      TO(L_MOUSE),  XXXXXXX,      KC_PSCR,      KC_F7,        KC_F8,        KC_F9,        KC_F12,       KC_CAPS,      \
-OSM(MOD_LSFT),XXXXXXX,      XXXXXXX,      TO(L_NAV),    TO(L_FUN),    XXXXXXX,      KC_SCRL,      KC_F4,        KC_F5,        KC_F6,        KC_F11,       CW_TOGG,      \
-OSM(MOD_LCTL),XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_PAUS,      KC_F1,        KC_F2,        KC_F3,        KC_F10,       OSM(MOD_RCTL),\
+KC_ESC,       TD(TD_RESET), XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_PSCR,      KC_F7,        KC_F8,        KC_F9,        KC_F12,       KC_DEL,       \
+KC_CAPS,      XXXXXXX,      XXXXXXX,      TO(L_NAV),    TO(L_FUN),    XXXXXXX,      KC_SCRL,      KC_F4,        KC_F5,        KC_F6,        KC_F11,       OSM(MOD_RSFT),\
+CW_TOGG,      U_P_UNDO,     U_P_CUT,      U_P_COPY,     U_P_PSTE,     XXXXXXX,      KC_PAUS,      KC_F1,        KC_F2,        KC_F3,        KC_F10,       OSM(MOD_RCTL),\
                                           KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_NAV \
-KC_ESC,       KC_HOME,      KC_UP,        KC_END,       KC_PGUP,      KC_VOLU,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LNG1,      \
+KC_ESC,       KC_HOME,      KC_UP,        KC_END,       KC_PGUP,      KC_VOLU,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_DEL,       \
 OSM(MOD_LSFT),KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_PGDN,      KC_VOLD,      XXXXXXX,      TO(L_NAV),    TO(L_FUN),    XXXXXXX,      XXXXXXX,      KC_LNG2,      \
-OSM(MOD_LCTL),U_P_UNDO,     U_P_CUT,      U_P_COPY,     U_P_PSTE,     KC_MUTE,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      OSM(MOD_RCTL),\
+OSM(MOD_LCTL),U_P_UNDO,     U_P_CUT,      U_P_COPY,     U_P_PSTE,     KC_MUTE,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LNG1,      \
                                           KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_SYML \
@@ -31,7 +31,7 @@ OSM(MOD_LCTL),KC_SLSH,      KC_PLUS,      KC_MINS,      KC_ASTR,      KC_PERC,  
                                           KC_TAB,       KC_SPC,       TO(L_SYML),   TO(L_BASE),   KC_BSPC,      KC_ENT
 
 #define HLOIREA_LAYER_MOUSE \
-KC_GRV,       KC_BTN2,      KC_MS_U,      KC_BTN1,      KC_BTN3,      KC_WH_U,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_DEL,       \
-OSM(MOD_LSFT),KC_MS_L,      KC_MS_D,      KC_MS_R,      XXXXXXX,      KC_WH_D,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      OSM(MOD_RSFT),\
+KC_ESC,       KC_BTN2,      KC_MS_U,      KC_BTN1,      KC_BTN3,      KC_WH_U,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_DEL,       \
+OSM(MOD_LSFT),KC_MS_L,      KC_MS_D,      KC_MS_R,      TO(L_MOUSE),  KC_WH_D,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      OSM(MOD_RSFT),\
 OSM(MOD_LCTL),XXXXXXX,      KC_ACL0,      KC_ACL1,      KC_ACL2,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      OSM(MOD_RCTL),\
                                           KC_TAB,       KC_SPC,       TO(L_BASE),   TO(L_BASE),   KC_BSPC,      KC_ENT
